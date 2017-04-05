5b11bfea0207cd15e5d00680a30df651372b6a68
Marcos Lee <marcosl8686@gmail.com>
2017-04-05 11:19:11 -0700

    add response if result comes out undefined

--- liri.js
+++ liri.js
@@ -37,13 +37,13 @@ function tweets() {
   }
 
   console.log("log.txt was updated!");
+  console.log("////////////////////////////////////////////////")
 
   });
 
 	var params = {screen_name: "@MarcosLee1",count: 14}
 	client.get("statuses/user_timeline", params, function(err, data, response){
 		if (!err) {
-      // console.log(response.body);
       for(var i = 0; i < 14; i++) {
         console.log(JSON.stringify("Tweet #" + (1 + i) + " " + data[i].text, null, 2));
       }
@@ -60,20 +60,39 @@ function spotifySong(input) {
   }
 
   console.log("log.txt was updated!");
+  console.log("////////////////////////////////////////////////")
 
   });
 	spotify.search({ type: 'track', query: input, limit: 1}, function(err, data) {
+    //check if track exist by looking at the length of found items.
+    var itemList = data.tracks.items.length;
 	    if (err) {
 	        console.log('Error occurred: ' + err);
 	        return;
 	    }
-	    else {
+      // if item list is greather than 0, use user input to provide data,
+	    if (itemList > 0) {
       console.log("Artist(s) name: " + data.tracks.items[0].artists[0].name);
       console.log("Song name: " + data.tracks.items[0].name);
       console.log("preview link: " + data.tracks.items[0].preview_url);
       console.log("Album name: " + data.tracks.items[0].album.name);
-
 	  	}
+      // if item list is less than 0, use pre defined query.
+      else {
+        spotify.search({ type: 'track', query: "The Sign, Ace of Base", limit: 1}, function(err, data) {
+        if (err) {
+          console.log('Error occurred: ' + err);
+          return;
+        }
+        else {
+          console.log("Artist(s) name: " + data.tracks.items[0].artists[0].name);
+          console.log("Song name: " + data.tracks.items[0].name);
+          console.log("preview link: " + data.tracks.items[0].preview_url);
+          console.log("Album name: " + data.tracks.items[0].album.name);
+        }
+
+        });
+      }
 	});
 }
 function movieSearch(input) {
@@ -86,14 +105,13 @@ function movieSearch(input) {
   }
 
   console.log("log.txt was updated!");
+  console.log("////////////////////////////////////////////////")
 
   });
-
   request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&r=json", function(error, response, body) {
 
   // If there were no errors and the response code was 200 (i.e. the request was successful)...
-    if (!error && response.statusCode === 200) {
-
+    if (!error && response.statusCode === 200 && body.length > 100) {
       console.log("Movie Title: " + JSON.parse(body).Title);
       console.log("Year: " + JSON.parse(body).Year);
       console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -102,6 +120,21 @@ function movieSearch(input) {
       console.log("Plot: " + JSON.parse(body).Plot);
       console.log("Actors: " + JSON.parse(body).Actors);
       console.log("Rotten Tomatoes Raing: " + JSON.parse(body).Ratings[1].Value);
+    }
+    //if a incorrect movie title is input, body length will be lower than 100, if so search for mr.nobdy.
+    else if(!error && response.statusCode === 200 && body.length < 100) {
+      request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&r=json", function(error, response, body) {
+      console.log("Movie Title: " + JSON.parse(body).Title);
+      console.log("Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("Rotten Tomatoes Raing: " + JSON.parse(body).Ratings[1].Value);
+
+      });
+
     }
   });
 }
@@ -114,6 +147,7 @@ function random() {
   }
 
   console.log("log.txt was updated!");
+  console.log("////////////////////////////////////////////////")
 
   });
   fs.readFile("random.txt", "utf8", function(error, data) {
